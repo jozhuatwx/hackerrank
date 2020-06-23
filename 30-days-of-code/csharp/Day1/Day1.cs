130473bf69859b82fdaa9783be1fb93a7eca65af
jozhuatwx <jozhuatwx@gmail.com>
2020-06-23 21:03:26 +0800

    day1

---
+++ 30-days-of-code/csharp/Day1/Day1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day1
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+            // print hello world
+            Console.WriteLine("Hello, World.");
+
+            // read input
+            string input = Console.ReadLine();
+
+            // print input statement
+            Console.WriteLine(input);
+        }
+    }
+}
