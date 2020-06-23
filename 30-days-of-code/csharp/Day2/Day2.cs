a485187c2afc12dce8a40797eb596a6aa3f88a68
jozhuatwx <jozhuatwx@gmail.com>
2020-06-23 21:48:26 +0800

    day2

---
+++ 30-days-of-code/csharp/Day2/Day2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day2
+{
+    class Day2
+    {
+        static int TotalCost(double meal_cost, int tip_percent, int tax_percent) {
+            // return total cost
+            return Convert.ToInt32(meal_cost + (meal_cost * tip_percent / 100) + (meal_cost * tax_percent / 100));
+        }
+
+        static void Main(string[] args)
+        {
+            // initialisation
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+            // print total cost statement
+            Console.WriteLine(TotalCost(meal_cost, tip_percent, tax_percent));
+        }
+    }
+}
